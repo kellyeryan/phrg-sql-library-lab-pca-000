ef4190b2c82a5521d6a6aaceded92be85ebaba66
kellyeryan <kellyeryan@gmail.com>
2019-10-01 09:34:58 -0400

    Finish library lab

---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO series (title) VALUES ("Rivers of London");
+INSERT INTO series (title) VALUES ("Harry Potter");
+INSERT INTO books (title, year) VALUES ("Midnight Riot", 2011);
+INSERT INTO books (title, year) VALUES ("Moon over Soho", 2011);
+INSERT INTO books (title, year) VALUES ("Whispers Underground", 2012);
+INSERT INTO books (title, year) VALUES ("The Philosopher's Stone", 1997);
+INSERT INTO books (title, year) VALUES ("The Chamber of Secrets", 1998);
+INSERT INTO books (title, year) VALUES ("The Prisoner of Azkaban", 1999);
+INSERT INTO characters (name, motto, species) VALUES ("Peter Grant", "Iustitia Omnibus", "human");
+INSERT INTO characters (name, motto, species) VALUES ("Lesley May", "Mundus Mutandus Est", "human");
+INSERT INTO characters (name, motto, species) VALUES ("Mr. Punch", "Nemo est Tutus", "revenant");
+INSERT INTO characters (name, motto, species) VALUES ("Beverley Brook", "Crescit Eundo", "river deity");
+INSERT INTO characters (name, motto, species) VALUES ("Harry Potter", "Draco numquam titillandus", "human");
+INSERT INTO characters (name, motto, species) VALUES ("Hedgewig", "Tutubam", "owl");
+INSERT INTO characters (name, motto, species) VALUES ("Hermione", "Sapientia", "human");
+INSERT INTO characters (name, motto, species) VALUES ("Moaning Myrtle", "Ululatus", "ghost");
+INSERT INTO subgenres (name) VALUES ("detective story");
+INSERT INTO subgenres (name) VALUES ("adventure");
+INSERT INTO authors (name) VALUES ("Ben Aaronovitch");
+INSERT INTO authors (name) VALUES ("JK Rowling");
+INSERT INTO character_books (book_id, character_id) VALUES (1, 1);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 3);
+INSERT INTO character_books (book_id, character_id) VALUES (3, 4);
+INSERT INTO character_books (book_id, character_id) VALUES (2, 3);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 3);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 4);
+INSERT INTO character_books (book_id, character_id) VALUES (2, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (4, 4);
+INSERT INTO character_books (book_id, character_id) VALUES (4, 5);
+INSERT INTO character_books (book_id, character_id) VALUES (5, 8);
+INSERT INTO character_books (book_id, character_id) VALUES (8, 6);
+INSERT INTO character_books (book_id, character_id) VALUES (7, 7);
+INSERT INTO character_books (book_id, character_id) VALUES (8, 7);
+INSERT INTO character_books (book_id, character_id) VALUES (7, 8);
+INSERT INTO character_books (book_id, character_id) VALUES (4, 8);
+
+
+
